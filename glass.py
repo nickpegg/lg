290c5e144c28d92d7cc5b9b41fab57204a2a1a12
Nick Pegg <nick@nickpegg.com>
2014-02-19 20:05:28 -0500

    Move the actual check to a different URL (fixes #2)

--- glass.py
+++ glass.py
@@ -10,34 +10,36 @@ app = Flask("lg")
 app.debug = True
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        result = None
-        output = ''
+    return render_template('index.html')
 
-        match = re.match("([\w\.\:\-\_]+)", request.form['target'])
-        if match:
-            target = match.group(1)
-            method = request.form['method']
 
-            log(request, method, target)
+@app.route('/go', methods=['POST'])
+def go():
+    result = None
+    output = ''
 
-            if method == 'ping':
-                result = ping(target)
-            elif method == 'mtr':
-                result = mtr(target)
-            else:
-                output = "You gave me an invalid method!"
+    match = re.match("([\w\.\:\-\_]+)", request.form['target'])
+    if match:
+        target = match.group(1)
+        method = request.form['method']
 
-            if result is not None:
-                output = result.stderr + result.stdout
+        log(request, method, target)
+
+        if method == 'ping':
+            result = ping(target)
+        elif method == 'mtr':
+            result = mtr(target)
         else:
-            output = "Your target doesn't look like an IP address or a hostname."
+            output = "You gave me an invalid method!"
 
-        return render_template('index.html', results=output)
+        if result is not None:
+            output = result.stderr + result.stdout
     else:
-        return render_template('index.html')
+        output = "Your target doesn't look like an IP address or a hostname."
+
+    return render_template('index.html', results=output)
 
 
 def ping(dest, count=10):
